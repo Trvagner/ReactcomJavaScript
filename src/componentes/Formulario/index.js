dd091eaa95da4809abe5bb3af3023d38aa0f96a4
Trvagner <116954836+Trvagner@users.noreply.github.com>
2023-01-28 05:41:29 -0300

    Atualização projeto v.4

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -3,11 +3,13 @@ import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
-const Formulario = () => {
+
+const Formulario = (props) => {
 
     const times = [
         'Programação',
         'Front-End',
+        'Data Science',
         'Devops',
         'Ux e Design',
         'Mobile',
@@ -21,6 +23,12 @@ const Formulario = () => {
     
     const aoSalvar = (evento) => {
         evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
         
     }
     
