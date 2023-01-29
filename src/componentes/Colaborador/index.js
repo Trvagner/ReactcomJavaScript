c23a4c35d4bdecbc473b7cb0ca0c48f68051f836
Trvagner <116954836+Trvagner@users.noreply.github.com>
2023-01-29 16:29:40 -0300

    Atualização do projeto v.5

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,19 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return( 
+        <div className='colaborador'>
+            <div className='cabecalho'>
+            <img src='https://github.com/Trvagner.png' alt='Vagner Torres'/>
+            </div>
+            <div className='rodape'>
+                <h4>Vagner Torres</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+                
+                      
+        </div>
+    )
+}
+
+export default Colaborador
